4772f3875047583d0bee1d1104196d6ab26d04cb
Denys Moroz <d.moroz.iv81@kpi.ua>
2024-02-21 19:36:34 +0000

    Finish custom template tags

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "blog"
 urlpatterns =  [
   path("",  views.IndexView.as_view(), name="main"),
+  path("post/<slug:slug>", views.PostDetail.as_view(), name="blog-post-detail"),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
+
 
 
 # Create your views here.
@@ -11,4 +12,11 @@ class IndexView(View):
   def get(self, request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     return render(request, "blog/index.html", {"posts": posts})
+
+
+class PostDetail(View):
+  def get(self, request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    context = {"post": post}
+    return render(request, "blog/post-detail.html", context)
 
