29f4a629358d5f9be3fefd0c593ee481c7f2633d
Denys Moroz <d.moroz.iv81@kpi.ua>
2024-02-23 22:24:20 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,20 @@
+import logging
+
 from django.shortcuts import render
 from django.views import View
 from django.shortcuts import render, get_object_or_404, redirect
-from blog.models import Post
 from django.utils import timezone
+
+from blog.models import Post
 from blog.forms import CommentForm
 
-
+logger = logging.getLogger(__name__)
 # Create your views here.
 
 class IndexView(View):
   def get(self, request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 
@@ -43,6 +47,7 @@ class PostDetail(View):
       comment.content_object = post
       comment.creator = request.user
       comment.save()
+      logger.info("Created comment on Post %d for user %s", post.pk, request.user)
       return redirect(request.path_info)
 
 
