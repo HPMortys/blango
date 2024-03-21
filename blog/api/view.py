8030ee04508328ec68aa91934cb0315efbf5276e
Denys Moroz <d.moroz.iv81@kpi.ua>
2024-03-21 11:10:41 +0000

    Finish permissions

--- blog/api/view.py
+++ blog/api/view.py
@@ -1,5 +1,5 @@
 from rest_framework import generics 
-
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from blog.api.serializers import PostSerializer
 from blog.models import Post
 from rest_framework.authentication import TokenAuthentication
@@ -13,3 +13,4 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
+  permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
